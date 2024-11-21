4ea24ac2f960cd989117f481941aa3ed9b956c61
Camille Yap <camille.yap2008@gmail.com>
2024-11-21 12:37:14 -0500

    edited functions in the python example

--- python_ex.py
+++ python_ex.py
@@ -1,15 +1,16 @@
 class ShoppingCart: 
+  '''Shopping cart of items labeled with prices'''
   def __init__(self):
     self.cart = None #replace None with cart
     self.prices = {"apple" : 1, "chicken" : 4, "broccoli" : 2, "baked beans" : 3}
 
   '''Adds {quantity} of {item} to the cart'''
   def add(self, item, quantity):
-    return None
+    pass
 
   '''Deletes {item} from the cart'''
   def delete(self, item):
-    return None
+    pass
 
   '''Returns the total cost of all the items in the cart'''
   def total_cost(self):
